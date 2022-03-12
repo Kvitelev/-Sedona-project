06871e923c7b7beacdc15077dcb05b05b0f1a833
Kvitelev Vasiliy <kvitelevv@gmail.com>
2022-03-12 14:49:24 +0300

    Вправки, анимация формы, localStorage для полей формы, минимализация файлов css и js, конец учебного проекта

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 const popup = document.querySelector('.window-hotel-search');
 const openPopupBtn = document.querySelector('.btn-search-hotel');
+const formHotelSearch = popup.querySelector('.form-hotel-search');
 const inputArrivalDate = popup.querySelector('.arrival-date');
+const inputDepartureDate = popup.querySelector('.departure-date');
+const userGrownup = popup.querySelector('.grownup');
+const userChildren = popup.querySelector('.children');
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("user");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 openPopupBtn.addEventListener('click', function () {
   popup.classList.toggle('window-hotel-search--disable');
+  if (storage) {
+    userGrownup.value = storage;
+    userChildren.value = storage;
+  }
   inputArrivalDate.focus();
 });
 
@@ -13,3 +29,14 @@ window.addEventListener('keydown', function (evt) {
     popup.classList.add('window-hotel-search--disable');
   }
 });
+
+formHotelSearch.addEventListener('submit', function (evt) {
+  if (!inputArrivalDate || !inputDepartureDate) {
+    evt.preventDefault();
+  }else {
+    if (isStorageSupport) {
+      localStorage.setItem('user', userGrownup.value);
+      localStorage.setItem('user', userChildren.value);
+    }
+  }
+})
